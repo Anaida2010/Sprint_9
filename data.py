0fc64d547b0c05485fe264c71c179bff42f9c36c
Anaida2010 <anaidka2010@yandex.ru>
2025-10-14 12:47:19 +0300

    Add files via upload
    файл с финальным исправлением

--- data.py
+++ data.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 
+photo_path = Path(__file__).parent / "assets" / "image.png"
+
 RECIPE_DATA = {
     "name": 'Бургер',
     "ingredient": 'булка',
     "amount": '1',
     "cooking_time": '15',
     "description": 'грустный бургер без котлетки',
-    "photo": str(Path("assets/image.png").resolve())
+    "photo": str(photo_path.resolve())
 }
